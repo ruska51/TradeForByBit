09ed83b51e8cd9dfd191c674e6d150f0d7afb789
ruska51 <96662968+ruska51@users.noreply.github.com>
2025-09-20 00:35:32 +0500

    Api nkey and Api Secret

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 import os
 
 
-API_KEY = os.getenv("BYBIT_API_KEY", "")
-API_SECRET = os.getenv("BYBIT_API_SECRET", "")
+API_KEY = os.getenv("BYBIT_API_KEY", "UAWJzEjAHx31UElsty")
+API_SECRET = os.getenv("BYBIT_API_SECRET", "7EPTFfv1kF9GCUgZUP3VyshBQJuwIKvDA7rT")
 
